44635486a4aaf6b6d5308840185ceb56736c7a2b
Dimitri Dhuyvetter <dimitri@dhuyvetter.net>
2016-09-08 12:05:46 +0200

    Add Integration Test

---
+++ tests/integration-tests/test-integration-sample.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Wcct16
+ */
+namespace WCCT16;
+/**
+ * Sample test case.
+ */
+class IntegrationTests extends \WP_UnitTestCase {
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
